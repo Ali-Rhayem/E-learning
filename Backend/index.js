1d96533d0f625d9c630038d4af796f4c4120650e
Alirh1 <alirhayem.1212@gmail.com>
2024-08-06 13:20:32 +0300

    added handleRegister functionality and solved the cors error in backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import connectDB from './connection.js';
 import authRoutes from './routes/auth.routes.js';
 import classRoutes from './routes/class.routes.js';
@@ -11,6 +12,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
