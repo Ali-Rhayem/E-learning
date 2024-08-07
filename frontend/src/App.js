43e31a73c7d64dd22e3e0af6604ab9139368a629
Alirh1 <alirhayem.1212@gmail.com>
2024-08-07 20:49:25 +0300

    added store to app.js and added classes pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import Register from './pages/Register/Register.jsx';
-import Login from './pages/Login/Login.jsx'
-import Home from './pages/home/home.jsx'
+import Login from './pages/Login/Login.jsx';
+import Home from './pages/home/home.jsx';
+import AllClasses from './pages/AllClasses/AllClasses.jsx';
+import MyClasses from './pages/MyClasses/MyClasses.jsx';
+import ClassFiles from './pages/ClassFiles';
 import Navbar from './components/Navbar/Navbar.jsx';
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <div>
-        <Routes>
-          <Route path="/Home" element={<Home/>}/>
-          <Route path="/Register" element={<Register/>}/>
-          <Route path="/Login" element={<Login/>}/>
-        </Routes>
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Navbar />
+        <div>
+          <Routes>
+            <Route path="/Home" element={<Home />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/all-classes" element={<AllClasses />} />
+            <Route path="/my-classes" element={<MyClasses />} />
+            <Route path="/class-files/:classId" element={<ClassFiles />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
